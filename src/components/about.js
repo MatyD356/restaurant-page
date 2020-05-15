eec82147070979bdffbea9c8bca51c54497046b6
Mateusz <matyd356@gmail.com>
2020-05-15 15:29:05 +0200

    add about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,7 +1,21 @@
+import Photo from "../assets/aboutImg.jpg"
+
 const about = (container) => {
     let aboutDiv = document.createElement("div");
     aboutDiv.classList.add("about");
     container.appendChild(aboutDiv);
-
+    // about img
+    let img = document.createElement("img");
+    img.src = Photo;
+    //title div
+    let title = document.createElement("h1");
+    title.classList.add("about-title");
+    title.textContent = "RESTAURANT";
+    aboutDiv.appendChild(title);
+    //text div
+    let text = document.createElement("p");
+    text.classList.add("aboutText");
+    text.textContent = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui dicta minus molestiae vel beatae natus eveniet ratione temporibus aperiam harum alias officiis assumenda officia quibusdam deleniti eos cupiditate dolore doloribus!";
+    aboutDiv.appendChild(text);
 }
 export { about };
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,19 +7,20 @@ const navbar = (container) => {
     container.appendChild(navDiv);
     //crearting logo
     let logo = document.createElement("img");
+    logo.classList.add("logo-img");
     logo.src = Icon;
     navDiv.appendChild(logo);
-    //about buttoon
+    //about button
     let aboutButton = document.createElement("button");
     aboutButton.classList.add("btn")
     aboutButton.textContent = "ABOUT";
     navDiv.appendChild(aboutButton);
-    //menu buttoon
+    //menu button
     let menuButton = document.createElement("button");
     menuButton.classList.add("btn")
     menuButton.textContent = "MENU";
     navDiv.appendChild(menuButton);
-    //contact buttoon
+    //contact button
     let contactButton = document.createElement("button");
     contactButton.classList.add("btn")
     contactButton.textContent = "CONTACT";
