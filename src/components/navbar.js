24194b74243156ab39fc5603b00846c3696c1012
Mateusz <matyd356@gmail.com>
2020-05-15 12:56:46 +0200

    add logo

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,14 @@
+import Icon from '../assets/logo.jpg';
+
 const navbar = (container) => {
     //creating navbar
     let navDiv = document.createElement("div");
     navDiv.classList.add("navbar");
     container.appendChild(navDiv);
     //crearting logo
+    let logo = document.createElement("img");
+    logo.src = Icon;
+    navDiv.appendChild(logo);
     console.log("navbar goes click");
 }
 export { navbar }
